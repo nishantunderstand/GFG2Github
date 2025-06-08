fa9a207789ac663a2bd538244544a6b9fd86ce95
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-06-08 21:31:02 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Activity Selection/activity-selection.java
@@ -0,0 +1,34 @@
+
+class Solution {
+    public int activitySelection(int[] start, int[] finish) {
+        PriorityQueue<int[]> pq = 
+            new PriorityQueue<>((a,b)-> Integer.compare(a[1],b[1]));
+        
+        for(int i=0;i<start.length;i++){
+            int[] pair = new int[2];
+            pair[0]=start[i];
+            pair[1]=finish[i];
+            pq.offer(pair);
+        }
+        
+        int lastEnd = 0;
+        int selectedJob = 0;
+        
+        while(!pq.isEmpty()){
+            int[] job = pq.poll();
+            if(lastEnd<job[0]){  //<-- EqualityCondition is Tricky
+                selectedJob++;
+                lastEnd= job[1];
+            }
+        }
+        return selectedJob;
+    }
+}
+
+
+/**
+    Length Mismatch => Error 
+    I need PriorityQueue
+    This is a Greedy Solution.
+    LeetCode had a Different Sol:=>
+*/
