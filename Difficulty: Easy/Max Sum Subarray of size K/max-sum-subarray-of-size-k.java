59a6744d6c452e628fe89bc3b4184cb4dddbb44e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-09-18 22:01:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -3,7 +3,7 @@ class Solution {
         int maxSum=0,windowSum=0,windowStart=0;
         for(int windowEnd=0;windowEnd<arr.length;windowEnd++){
             windowSum += arr[windowEnd];
-            if(windowEnd>=k-1){
+            if(windowEnd>=k-1){ //<-- 
                 maxSum = Math.max(windowSum,maxSum);
                 windowSum -= arr[windowStart];
                 windowStart++;
