7d2ecfa93afafe75d1905d360e875716bf4b8f99
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-26 23:32:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common Elements/common-elements.java
+++ Difficulty: Easy/Common Elements/common-elements.java
@@ -2,6 +2,40 @@
 
 class Solution {
     public static ArrayList<Integer> commonElements(int a[], int b[]) {
+        //return hashMapApproach(a,b);
+        return twoPointerApproach(a,b);
+    }
+    
+    public static ArrayList<Integer> twoPointerApproach(int a[], int b[]) {
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        int i = 0;
+        int j = 0;
+        
+        // How to handle Duplicates ???
+        
+        while(i<a.length && j<b.length){
+            if(a[i]==b[j]){
+                res.add(a[i]);
+                i++;
+                j++;
+            }
+            else if(a[i]>b[j]){
+                j++;
+            }else if(a[i]<b[j]){
+                i++;
+            }
+        }
+        
+        return res;
+    }
+    
+    
+    
+    public static ArrayList<Integer> hashMapApproach(int a[], int b[]) {
         ArrayList<Integer> res = new ArrayList<>();
         Map<Integer, Integer> fMap = new HashMap<>();
         
