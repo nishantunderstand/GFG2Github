295b1b31c69458b791add08e14d59ae92e256e37
Nishant Raj <nishant.read@gmail.com>
2026-01-05 23:14:06 +0530

    Monday, January 5, 2026 11:14:00 PM
    Monday, January 5, 2026 11:14:04 PM

--- Difficulty Medium/Zero Sum Subarrays/zero-sum-subarrays.java
+++ Difficulty Medium/Zero Sum Subarrays/zero-sum-subarrays.java
@@ -1,36 +1,4 @@
-//{ Driver Code Starts
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt(); // Number of test cases
-        sc.nextLine();        // Consume the newline
-
-        while (t-- > 0) {
-            String input = sc.nextLine(); // Input array as a space-separated string
-            String[] inputArray = input.split(" ");
-            int[] arr = new int[inputArray.length];
-
-            // Parse the input array
-            for (int i = 0; i < inputArray.length; i++) {
-                arr[i] = Integer.parseInt(inputArray[i]);
-            }
-
-            Solution ob = new Solution();
-            System.out.println(ob.findSubarray(arr));
-            System.out.println("~");
-        }
-
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-
+// Zero Sum Subarrays
 class Solution {
     public int findSubarray(int[] arr) {
         return prefixHashMapApproach(arr);
@@ -40,25 +8,18 @@ class Solution {
     // Time Complexity:O(n) | Space Complexity:O(n)
     public int prefixHashMapApproach(int[] arr) {
         int cnt = 0;
-        int i = -1;
         int csum = 0;
-
         // Sum-i
         HashMap<Integer, Integer> hm = new HashMap<>();
         hm.put(0,1);
-        
-        while(i<arr.length-1){ //<--
-            i++;
+        for(int i=0;i<arr.length;i++){
             csum+=arr[i];
-            
-            
             if(hm.containsKey(csum)){
                 cnt += hm.get(csum);
             }
             
             hm.put(csum,hm.getOrDefault(csum,0)+1);
         }
-        
         return cnt;
     }
 }
