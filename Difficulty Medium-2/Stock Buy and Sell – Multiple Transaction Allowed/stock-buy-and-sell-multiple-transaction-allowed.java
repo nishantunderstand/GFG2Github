a0053d0eac9e52a9d234b4358a320c5a0844122e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-10 15:24:18 +0000

    Restore accidentally deleted files

--- Difficulty Medium-2/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.java
+++ Difficulty Medium-2/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.java
@@ -1,11 +1,23 @@
-// User function Template for Java
-
 // Stock Buy and Sell – Multiple Transaction Allowed
-
 class Solution {
     public int maximumProfit(int prices[]) {
+        //return maximumProfit1(prices);
+        return maximumProfit2(prices);
+    }
+    private int maximumProfit2(int[] prices){
+        if(prices==null || prices.length<1) return 0;
         int maxProfit = 0;
-        
+        for(int i=1;i<prices.length;i++){
+            if(prices[i-1]<prices[i]){
+                maxProfit += prices[i] - prices[i-1];
+            }
+        }
+        return maxProfit;
+    }
+    
+    
+    private int maximumProfit1(int prices[]) {
+        int maxProfit = 0;
         for(int i=1;i<prices.length;i++){
             if(prices[i]>prices[i-1]){
                 maxProfit += prices[i]-prices[i-1];
@@ -20,6 +32,4 @@ class Solution {
 Profit : Buy at low and Sell at max 
 How this is Different from Previous Question ?
 That now we can sell at different.
-
-
 */
