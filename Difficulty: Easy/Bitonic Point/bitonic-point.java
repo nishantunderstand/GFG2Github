221be83ad1a55462d2e3f928434dff30d8117769
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:40:25 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+// Bitonic Point GFG
+class Solution {
+    public int findMaximum(int[] arr) {
+        int start = 0;
+        int end =  arr.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            
+            // Increasing Slope
+            if(arr[mid]<arr[mid+1]){
+                start = mid+1;
+            }
+            // Decreasing Slope
+            else {
+                end = mid;
+            }
+        }
+        return arr[start];
+    }
+}
+// Runtime Erorr
