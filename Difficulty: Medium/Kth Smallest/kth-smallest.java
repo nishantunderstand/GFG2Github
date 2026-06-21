00851aba81a417d15e3423d5d2e8920e30a49e4b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-21 07:13:45 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int kthSmallest(int[] arr, int k) {
+        //return heapApproach(arr,k);
+        return bruteForce(arr,k);
+    }
+    private int bruteForce(int[] arr, int k){
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+    
+    private int heapApproach(int[] arr, int k) {
+        // Max Priority Queue 
+        PriorityQueue<Integer> maxPQ = 
+            new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<arr.length;i++){
+            maxPQ.offer(arr[i]);
+            if(maxPQ.size()>k){
+                maxPQ.poll();
+            }
+        }
+        return maxPQ.poll();
+    }
+}
