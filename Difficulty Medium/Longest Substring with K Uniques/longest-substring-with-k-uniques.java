c3b9df8d9985427c01995512447c71f10942e0bb
Nishant Raj <nishant.read@gmail.com>
2026-07-05 12:17:48 +0530

    LenovoBat 05-Jul-26 12:17:48.79

--- Difficulty Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -1,29 +1,30 @@
+// GFG : Longest Substring with K Uniques
+// Question : When I need Frequency Or Index
+
+// Saturday, July 4, 2026 4:31:24 PM
+// TC : O(n) | SC : O(n)
 class Solution {
     public int longestKSubstr(String s, int k) {
         Map<Character, Integer> fMap = new HashMap<>();
-        
-        int windowStart=0;
-        int maxLen = -1;
-        
-        for(int windowEnd=0; windowEnd<s.length();windowEnd++){
-            char rightChar = s.charAt(windowEnd);
-            // Expand
+        int left = 0;
+        int maxLen = -1; //<--
+        for(int right=0;right<s.length();right++){
+            char rightChar = s.charAt(right);
             fMap.put(rightChar,fMap.getOrDefault(rightChar,0)+1);
             
-            // Shrink
             while(fMap.size()>k){
-                char leftChar = s.charAt(windowStart);
+                char leftChar = s.charAt(left);
                 fMap.put(leftChar,fMap.get(leftChar)-1);
                 if(fMap.get(leftChar)==0){
                     fMap.remove(leftChar);
                 }
-                windowStart++;
+                left++;
             }
+            
             if(fMap.size()==k){
-               maxLen = Math.max(maxLen, windowEnd-windowStart+1); 
+                maxLen = Math.max(maxLen, right-left+1);    
             }
         }
         return maxLen;
-        
     }
 }
