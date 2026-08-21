2deb300567815968d1c3214c48cd52b4bf09120e
Nishant Raj <nishant.read@gmail.com>
2026-08-21 11:26:27 +0530

    Friday, August 21, 2026 11:26:21 AM

--- Difficulty Easy/Find Kth Rotation/find-kth-rotation.java
+++ Difficulty Easy/Find Kth Rotation/find-kth-rotation.java
@@ -1,20 +1,13 @@
 class Solution {
     public int findKRotation(int arr[]) {
-        int low = 0;
-        int high = arr.length-1;
-        while(low<high){ //<--
-            int mid = low + (high-low)/2;
-            
-            if(arr[mid]>arr[high]){
-                low = mid+1;
-            }else{
-                high = mid; //<--
+        int minIdx = 0;
+        int minVal = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<minVal){
+                minIdx = i;
+                minVal = arr[i];
             }
         }
-        return low;
+        return minIdx;
     }
 }
-
-// Sunday, June 14, 2026 5:49:56 PM
-// TC : O(logn) | SC : O(1)
-// GFG : Find Kth Rotation
